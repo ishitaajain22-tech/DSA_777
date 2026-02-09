54afbf286d8a4ac684dbb3851982a4fc774ade46
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-02-09 13:25:33 +0530

    subset sum solution

--- DP/one.cpp
+++ DP/one.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-using namespace std;
+// LEETCODE/GFG -> PRACTICE LINK ( https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1 )
 
-int main() {
-    int t;
-    cin >> t;
-    
-    while(t--) {
-        int n, k, p;
-        cin >> n >> k >> p;
-        
-        if(abs(k) > n * p) {
-            cout << -1 << endl;
-            continue;
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n=arr.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true; //baaki sab already initialzed by false
         }
-        int ops = (abs(k) + p - 1) / p; 
-        ops = min(ops, n); 
         
-        cout << ops << endl;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(j < arr[i-1]){
+                    dp[i][j] = dp[i-1][j]; //bada element will be xcluded always
+                }else{
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]]; //or because bool values ke saath dealing
+                }
+            }
+        }
+        return dp[n][sum];
     }
-    
-    return 0;
-}
+};
