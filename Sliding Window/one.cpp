aa562d096d522813d0842ff65892419d7d350303
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-02-09 13:54:15 +0530

    Add problem references in one.cpp
    Added comments for problem references related to max sum of distinct subarrays.

---
+++ Sliding Window/one.cpp
@@ -0,0 +1,2 @@
+// max sum of distinct subarrays with len K -> https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/
+//max sum of subarrays with len K -> https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
