8fbaa7ea4c19a1964124059f6bd5d6493ccb7a9f
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-02-12 12:24:13 +0530

    FInd K Closest Elements

---
+++ Heaps/4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>>pq;
+        for(int i=0;i<arr.size();i++){
+            int dist = abs(arr[i] - x);
+            pq.push({dist,arr[i]});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        vector<int>res;
+        for(int i=0;i<k;i++){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(res.begin(),res.end()); //this is also important
+        return res;
+    }
+};
