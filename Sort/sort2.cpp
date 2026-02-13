1ee3ae67da6fa26bc3b1c71d46eb7a34a405f3a8
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-02-13 18:55:02 +0530

    sort arr by increasing freq

--- Sort/sort2.cpp
+++ Sort/sort2.cpp
@@ -7,7 +7,7 @@ public:
         for(int num:nums){
             mp[num]++;
         }
-        sort(nums.begin(),nums.end(),[&](int a,int b){ //repeatedly asks should n1 come before n2
+        sort(nums.begin(),nums.end(),[&](int a,int b){ //repeatedly sorting algo asks should n1 come before n2
             if(mp[a] ==  mp[b]){
                 return mp[a]>mp[b];
             }
