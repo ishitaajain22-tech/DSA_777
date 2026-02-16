a7979d0bafe0c0bd40b31e7083caefa894bb12c0
Ishita Jain <216325024+ishitaajain22-tech@users.noreply.github.com>
2026-02-16 13:50:31 +0530

    frequency sort

---
+++ Heaps/7.cpp
@@ -0,0 +1,18 @@
+//problem link: https://leetcode.com/problems/sort-array-by-increasing-frequency/
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int num:nums){
+            mp[num]++;
+        }
+        sort(nums.begin(),nums.end(),[&](int a,int b){ //repeatedly asks should n1 come before n2
+            if(mp[a] ==  mp[b]){
+                return mp[a]>mp[b];
+            }
+            return mp[a]<mp[b];
+        });
+        return nums;
+    }
+};
